b6d47bd8a2e543d17e8b3934e7d4d459acf93747
whoisYeshua <alien19987@gmail.com>
2025-12-14 10:20:31 +0300

    fix: update GraphQL server function file extension from .js to .ts

--- backend/graphql-server/api/graphql.ts
+++ backend/graphql-server/api/graphql.ts
@@ -1,4 +1,4 @@
-import { yoga } from '../src/server.ts'
+import { yoga } from '../dist/server.js'
 
 // export default function handler(req: VercelRequest, res: VercelResponse) {
 //   const { name = 'World' } = req.query
